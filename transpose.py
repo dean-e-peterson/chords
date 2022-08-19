16f36997125c0871d95eee15b5dd940a195ab31e
Dean E. Peterson <dean.e.penguin@gmail.com>
2022-08-18 22:54:34 -0400

    Add transpose.py script

---
+++ transpose.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+import re
+import sys
+
+sharps = ('A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#')
+flats = ('A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab')
+# m for minor, a for add, d for dim, and s for sus
+patternNotes = r'(?:^|(?<=[ /]))[A-G][#b]?(?:$|(?=[ mads/\d]))'
+
+
+def transpose_file(file, halfsteps):
+    for line in file:
+        print(transpose_line(line, halfsteps), end = '')
+
+
+def transpose_line(line, halfsteps):
+    def replace_note(match_note):
+        note = match_note.group(0)
+        return transpose_note(note, halfsteps)
+
+    return re.sub(patternNotes, replace_note, line)
+
+
+def transpose_note(note, halfsteps):
+    if note in sharps:
+        i = sharps.index(note)
+        #print(f'Sharp[{i}] {note} => ', end = '')
+        i = (i + halfsteps) % 12
+        note = sharps[i]
+        #print(f'Sharp[{i}] {note}')
+    elif note in flats:
+        i = flats.index(note)
+        #print(f'Flat[{i}] {note} => ', end = '')
+        i = (i + halfsteps) % 12
+        note = flats[i]
+        #print(f'Flat[{i}] {note}')
+
+    return note
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) < 2:
+        exit(1)
+
+    halfsteps = int(sys.argv[1])
+
+    filename = None
+    if len(sys.argv) > 2:
+        filename = sys.argv[2]
+
+    if filename == None:
+        file = sys.stdin
+    else:
+        file = open(filename, 'rt')
+
+    transpose_file(file, halfsteps)
